c42261010f461a16374f777f7570c30c10406510
eloso85 <eloso85@gmail.com>
2023-10-22 15:27:15 -0500

    made seconf ticket

--- app/(components)/TicketForm.jsx
+++ app/(components)/TicketForm.jsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
 const TicketForm = () => {
+  const router = useRouter();
   const handleChange = (e) => {
     const value = e.target.value;
     const name = e.target.name;
@@ -14,8 +15,18 @@ const TicketForm = () => {
     }));
   };
 
-  const handleSubmit = () => {
-    console.log("Submitted");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch("/api/Tickets", {
+      method: "POST",
+      body: JSON.stringify({ formData }),
+      "content-type": "applictaion/json",
+    });
+    if (!res.ok) {
+      throw new Error("Failed to create Ticket");
+    }
+    router.refresh();
+    router.push("/");
   };
 
   const startingTicketData = {
